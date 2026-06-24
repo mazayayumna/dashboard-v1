cb2d1b57df3dcc2a30ae52adf0c5fcc7b20f2d8a
Mazaya Yumna <mazayayumnawijaya@gmail.com>
2026-06-24 14:59:23 +0700

    add placeholder connection script

--- connection.py
+++ connection.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
+import configparser
 
-DATABASE_URL = "postgresql://postgres:PASS@localhost:5432/disease_dashboard"
+config = configparser.ConfigParser(strict=False)
+config.read("/etc/config.ini")
+
+DATABASE_URL = 'postgresql://' + config['dashboard_dummy']['user'] + ':' + config['dashboard_dummy']['pass'] + '@' + config['dashboard_dummy']['host'] + ':' + config['dashboard_dummy']['port'] + '/' + config['dashboard_dummy']['db']
 
 engine = create_engine(DATABASE_URL)
